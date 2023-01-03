d4b7408507fe391a839811b6c3bbeb8beafb72f1
MShahryiar <muhammadshahryiar@yahoo.com>
2023-01-03 11:07:12 +0500

    Authentication/Auth, UserModel

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js"
 
 
 // Configurations
@@ -37,15 +38,16 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({storage})
-
-
+// ROUTE WITH FILES 
+// Route, middleware (to store images locally), controller
+app.post('/auth/register', upload.single("picture"), register)
 // Mongoose
 const PORT = process.env.PORT || 6001
 mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParse:true,
+    useNewUrlParser:true,
     useUnifiedTopology:true,
 })
 .then(()=>{
-    app.listen(PORT,()=> console.log(`Server Port ${PORT}`))
+    app.listen(PORT,()=> console.log(`Server running on Port : ${PORT}`))
 })
 .catch((error)=> console.log(`${error} did not connect`))
